448e11616f6138c53dcfd98e5a489564de8fe3f6
Naima Nusrat Jafrin <102369761+Jafrin92@users.noreply.github.com>
2025-03-13 00:31:57 +0600

    slide

---
+++ js/script.js
@@ -0,0 +1,72 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let slides = document.querySelector(".slides");
+  let dotsContainer = document.querySelector(".dots");
+  let prevButton = document.querySelector(".prev");
+  let nextButton = document.querySelector(".next");
+  let slide = document.querySelectorAll(".slide");
+  let slideCount = slide.length;
+
+  let currentSlide = 0;
+
+  // Create dots
+  for (let i = 0; i < slideCount; i++) {
+    let dot = document.createElement("span");
+    if (i === 0) {
+      dot.classList.add("active");
+    }
+    dot.classList.add("dot");
+    dot.setAttribute("data-index", i);
+    dotsContainer.appendChild(dot);
+  }
+
+  let dots = document.querySelectorAll(".dot");
+
+  //slider change index
+  function updateSlider(index) {
+    if (index >= slideCount) {
+      currentSlide = 0;
+    } else if (index < 0) {
+      currentSlide = slideCount - 1;
+    } else {
+      currentSlide = index;
+    }
+
+    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+    dots.forEach((dot) => dot.classList.remove("active"));
+    dots[currentSlide].classList.add("active");
+  }
+
+  //   button
+
+  prevButton.addEventListener("click", function () {
+    updateSlider(currentSlide - 1);
+  });
+
+  nextButton.addEventListener("click", function () {
+    updateSlider(currentSlide + 1);
+  });
+
+  //   dot click
+
+  dots.forEach((dot) => {
+    dot.addEventListener("click", function () {
+      let index = parseInt(dot.getAttribute("data-index"));
+      updateSlider(index);
+    });
+  });
+
+  //   auto slide
+  let autoSlide = setInterval(function () {
+    updateSlider(currentSlide + 1);
+  }, 2000);
+
+  slides.addEventListener("mouseenter", function () {
+    clearInterval(autoSlide);
+  });
+
+  slides.addEventListener("mouseleave", function () {
+    autoSlide = setInterval(function () {
+      updateSlider(currentSlide + 1);
+    }, 2000);
+  });
+});
